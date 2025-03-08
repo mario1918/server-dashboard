2b2f149c9d27374be49f569dcf9e8a1c5de3d837
Mario Ashraf <mario.ashraf@siliconexpert.com>
2025-03-08 13:53:03 +0200

    adding ngDestoy to the interval function

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -9,16 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  // private interval?: ReturnType<typeof setInterval>;
 
-  constructor() {
+  constructor(private destroyRef: DestroyRef) {
     
   }
 
   ngOnInit() {
-    setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random();
       this.currentStatus = rnd < 0.5 ? 'online' : rnd < 0.9 ? 'offline' : 'unknown';
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
+
+  // ngOnDestroy() {
+  //   if (this.interval) {
+  //     clearInterval(this.interval);
+  //   }
+  // }
 
 }
